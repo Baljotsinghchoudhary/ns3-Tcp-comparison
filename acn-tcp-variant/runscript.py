35c82671f81a5b86b175acc0bf959f1dc954bd2b
BALJOT <64342553+Baljotsinghchoudhary@users.noreply.github.com>
2021-04-26 13:24:11 +0530

    Update runscript.py

--- acn-tcp-variant/runscript.py
+++ acn-tcp-variant/runscript.py
@@ -1,7 +1,6 @@
-            #running tcp variants one by one
-        import os
-
-        tcp_variants=["TcpNewReno", "TcpHybla", "TcpHighSpeed","TcpHtcp","TcpVegas", "TcpScalable","TcpVeno" ,"TcpBic", "TcpYeah", "TcpIllinois", "TcpWestwood", "TcpWestwoodPlus", "TcpLedbat"]
-        for protocol in tcp_variants:
-            os.system(f'./waf --run "scratch/acn-tcp-variant/acn-tcp-variant --transport_prot={protocol} --error_p=0.00 --cbr=0" ')
+#running tcp variants one by one
+import os
+tcp_variants=["TcpNewReno", "TcpHybla", "TcpHighSpeed","TcpHtcp","TcpVegas", "TcpScalable","TcpVeno" ,"TcpBic", "TcpYeah", "TcpIllinois", "TcpWestwood", "TcpWestwoodPlus", "TcpLedbat"]
+for protocol in tcp_variants:
+    os.system(f'./waf --run "scratch/acn-tcp-variant/acn-tcp-variant --transport_prot={protocol} --error_p=0.00 --cbr=0" ')
             
